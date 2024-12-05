bffab3f2551839dc04643fe85a6b56aa32b5a565
Sean Urgel <developer@seanurgel.dev>
2024-12-05 18:51:18 -0500

    chore(config): install husky

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      ["api", "config", "db", "tooling", "ui", "validators"],
+    ],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "analytics",
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "rfc",
+      ],
+    ],
+  },
+};
